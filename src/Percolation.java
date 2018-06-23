5b4f9379cd0b0c9700bf7c617f6e1de640510b8e
andela-bomotoso <bukola.omotoso@andela.com>
2018-06-22 23:26:10 -0500

    solution to cousera algorithms Percolation Problem

---
+++ src/Percolation.java
@@ -0,0 +1,119 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private static final int OPENED = 1;
+    private static final int CLOSED = 0;
+    private final int n;
+    private int opensites;
+    private int[][] site;
+    private final int virtualTopsite;
+    private final int virtualBottomsite;
+    private final WeightedQuickUnionUF weightedQuickUnionUF;
+
+    public Percolation(int n) {
+        validate(n);
+        this.n = n;
+        site = new int[n][n];
+        opensites = 0;
+        for (int i = 0; i < n; i++)
+
+            for (int j = 0; j < n; j++)
+                site[i][j] = CLOSED;
+
+        this.weightedQuickUnionUF = new WeightedQuickUnionUF((n * n) + 2);
+        virtualTopsite = 0;
+        virtualBottomsite = n * n + 1;
+    }
+
+    // Open a closed site and connect it with all neighbouring open sites
+    public void open(int row, int col) {
+        validate(row, col);
+        if (!isOpen(row, col)) {
+            row = row - 1;
+            col = col - 1;
+            site[row][col] = OPENED;
+            opensites++;
+            if (isFirstRow(row)) {
+                // Connect to imaginary top site
+                weightedQuickUnionUF.union(virtualTopsite, getSiteId(row, col));
+            }
+            if (isLastRow(row)) {
+                // connect to imaginary bottom site
+                weightedQuickUnionUF.union(virtualBottomsite, getSiteId(row, col));
+            }
+            // check if left side of the curent site is opened and perform a connection
+            if (!isFirstCol(col) && (site[row][col - 1]) == OPENED) {
+                weightedQuickUnionUF.union(getSiteId(row, col), getSiteId(row, col - 1));
+            }
+            // check if right side of the curent site is opened and perform a connection
+            if (!isLastCol(col) && (site[row][col + 1]) == OPENED) {
+                weightedQuickUnionUF.union(getSiteId(row, col), getSiteId(row, col + 1));
+            }
+            // check if top side of the curent site is opened and perform a connection
+            if (!isFirstRow(row) && (site[row - 1][col]) == OPENED) {
+                weightedQuickUnionUF.union(getSiteId(row, col), getSiteId(row - 1, col));
+            }
+            // check if bottom side of the curent site is opened and perform a connection
+            if (!isLastRow(row) && (site[row + 1][col]) == OPENED) {
+                weightedQuickUnionUF.union(getSiteId(row, col), getSiteId(row + 1, col));
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return site[row - 1][col - 1] == 1;
+    }
+
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return (isOpen(row, col) && weightedQuickUnionUF.connected(getSiteId(row, col), 0));
+    }
+
+    public int numberOfOpenSites() {
+        return opensites;
+    }
+
+    public boolean percolates() {
+
+        return weightedQuickUnionUF.connected(virtualTopsite, virtualBottomsite);
+    }
+
+    private void validate(int row, int col) {
+        if (row - 1 < 0 || row - 1 >= n) {
+            throw new IllegalArgumentException("row " + row + " is not between 0 and " + (n - 1));
+        }
+        if (col - 1 < 0 || col - 1 >= n) {
+            throw new IllegalArgumentException("column " + col + " is not between 0 and " + (n - 1));
+        }
+    }
+
+    private void validate(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException(n + " must be greater than 0 ");
+        }
+    }
+
+    private int getSiteId(int row, int col) {
+        // Based on sites with zero indexes
+        return (n * row) + col + 1;
+    }
+
+    private boolean isFirstCol(int col) {
+        return col == 0;
+    }
+
+    private boolean isLastCol(int col) {
+        return col == n - 1;
+    }
+
+    private boolean isFirstRow(int row) {
+        return row == 0;
+    }
+
+    private boolean isLastRow(int row) {
+        return row == n - 1;
+    }
+
+}
+
---
+++ src/PercolationStats.java
@@ -0,0 +1,63 @@
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class PercolationStats {
+    private static final double CONFIDENCE_95 = 1.96;
+    private double CONFIDENCE_STDDEV;
+    private final int trials;
+    private double[] thresholds;
+
+    public PercolationStats(int n, int trials) {
+        validate(n, trials);
+        this.trials = trials;
+        this.thresholds = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+            // System.out.print("Test " + (i + 1));
+            while (!percolation.percolates()) {
+                int randomRow = StdRandom.uniform(n) + 1;
+                int randomCol = StdRandom.uniform(n) + 1;
+                percolation.open(randomRow, randomCol);
+            }
+            // System.out.print(": Open Sites " + percolation.numberOfOpenSites() + "\n");
+            thresholds[i] = ((double) percolation.numberOfOpenSites()) / (n * n);
+        }
+        CONFIDENCE_STDDEV = CONFIDENCE_95 * stddev();
+    } // perform trials independent experiments on an n-by-n grid
+
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }                      // sample mean of percolation threshold
+
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+
+    }                      // sample standard deviation of percolation threshold
+
+    public double confidenceLo() {
+        return mean() - (CONFIDENCE_STDDEV / Math.sqrt(trials));
+    }                // low  endpoint of 95% confidence interval
+
+    public double confidenceHi() {
+        return mean() + (CONFIDENCE_STDDEV / Math.sqrt(trials));
+    } // high endpoint of 95% confidence interval
+
+    private void validate(int n, int trials) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n cannot be " + n + ",it must be greater than 0 ");
+        }
+        if (trials <= 0) {
+            throw new IllegalArgumentException("trials cannot be " + trials + ",it must be greater than 0 ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats percolationStats = new PercolationStats(n, trials);
+        System.out.printf("%-30s%s%f\n", "mean", "=", percolationStats.mean());
+        System.out.printf("%-30s%s%f\n", "stddev", "=", percolationStats.stddev());
+        System.out.printf("%-30s%s%s%f%s%f%s\n", "95% confidence interval", "=",
+                "[", percolationStats.confidenceLo(), ",", percolationStats.confidenceHi(), "]");
+    }
+}
